11b20b8e2a4db86971d1b8bb2aad49d873afa7af
fyquah <fyquah@protonmail.com>
2025-12-07 18:43:11 +0000

    Ocamlformat

--- test/stream_simulator.ml
+++ test/stream_simulator.ml
@@ -1,97 +1,93 @@
 open Core
-open Hardcaml 
-
+open Hardcaml
 module Stream = Xls_demo_lib.Stream
 module Step = Hardcaml_step_testbench_effectful.Imperative.Cyclesim
-module Before_and_after_edge = Hardcaml_step_testbench_effectful.Before_and_after_edge
+
+module Before_and_after_edge =
+  Hardcaml_step_testbench_effectful.Before_and_after_edge
 
 module Send_ports = struct
-  type t =
-    { source : Bits.t ref Stream.With_valid.t
-    ; ready  : Bits.t ref Before_and_after_edge.t
-    }
+  type t = {
+    source : Bits.t ref Stream.With_valid.t;
+    ready : Bits.t ref Before_and_after_edge.t;
+  }
 end
 
 module Receive_ports = struct
-  type t =
-    { source : Bits.t ref Stream.With_valid.t Before_and_after_edge.t 
-    ; ready  : Bits.t ref 
-    }
+  type t = {
+    source : Bits.t ref Stream.With_valid.t Before_and_after_edge.t;
+    ready : Bits.t ref;
+  }
 end
 
 type config =
-  | Always_high 
+  | Always_high
   | Rng of { rng : Random.State.t; probability_high : float }
 
 let sample_from_config config =
-  match config with 
-  | Always_high -> true 
+  match config with
+  | Always_high -> true
   | Rng { rng; probability_high } ->
-    Float.(<) (Random.State.float rng 1.0) probability_high
+      Float.( < ) (Random.State.float rng 1.0) probability_high
 
-let default_config = Rng { rng = Random.State.default ;probability_high = 0.95 }
+let default_config = Rng { rng = Random.State.default; probability_high = 0.95 }
 
-let send_packet ?(config = default_config) (h @ local) { Send_ports.source; ready } (packet : string) =
+let send_packet ?(config = default_config) (h @ local)
+    { Send_ports.source; ready } (packet : string) =
   let num_words = (String.length packet + 3) / 4 in
-  let get x =
-    if x < String.length packet then 
-      Char.to_int packet.[x]
-    else
-      0
-  in
+  let get x = if x < String.length packet then Char.to_int packet.[x] else 0 in
   let tkeep_last =
     let l = String.length packet mod 4 in
-    Bits.of_unsigned_int ~width:4
-      (if l = 0 then 0xF else (1 lsl l) - 1)
+    Bits.of_unsigned_int ~width:4 (if l = 0 then 0xF else (1 lsl l) - 1)
   in
   for i = 0 to num_words - 1 do
-    let ch3 = get (i * 4 + 3) in
-    let ch2 = get (i * 4 + 2) in
-    let ch1 = get (i * 4 + 1) in
-    let ch0 = get (i * 4 + 0) in
-    while not (sample_from_config (config)) do
-      Step.cycle h ();
+    let ch3 = get ((i * 4) + 3) in
+    let ch2 = get ((i * 4) + 2) in
+    let ch1 = get ((i * 4) + 1) in
+    let ch0 = get ((i * 4) + 0) in
+    while not (sample_from_config config) do
+      Step.cycle h ()
     done;
     source.valid := Bits.vdd;
-    source.value.tdata  := Bits.of_unsigned_int ~width:32 ((ch3 lsl 24) + (ch2 lsl 16) + (ch1 lsl 8) + ch0);
+    source.value.tdata :=
+      Bits.of_unsigned_int ~width:32
+        ((ch3 lsl 24) + (ch2 lsl 16) + (ch1 lsl 8) + ch0);
     source.value.tfirst := Bits.of_bool (i = 0);
-    source.value.tlast  := Bits.of_bool (i = num_words - 1);
-    source.value.tkeep  := (if i = num_words - 1 then tkeep_last else Bits.ones 4);
+    source.value.tlast := Bits.of_bool (i = num_words - 1);
+    source.value.tkeep := if i = num_words - 1 then tkeep_last else Bits.ones 4;
     Step.cycle h ();
     while not (Bits.to_bool !(ready.before_edge)) do
-      Step.cycle h ();
+      Step.cycle h ()
     done;
-    source.valid := Bits.gnd;
-  done;
-;;
+    source.valid := Bits.gnd
+  done
 
-let receive_packet ?(config = default_config) (h @ local) (ports : Receive_ports.t) =
+let receive_packet ?(config = default_config) (h @ local)
+    (ports : Receive_ports.t) =
   let collected = Vec.create () in
   let is_first_word = ref true in
   let rec loop () =
     ports.ready := Bits.gnd;
-    while (not (sample_from_config config)) do
-      Step.cycle h ();
+    while not (sample_from_config config) do
+      Step.cycle h ()
     done;
     (* Now, spin until we algin with a valid pulse *)
-    while 
+    while
       Step.cycle h ();
       not (Bits.to_bool !(ports.source.before_edge.valid))
     do
       ()
     done;
-    let stream = Stream.With_valid.map ~f:(!) ports.source.before_edge in
+    let stream = Stream.With_valid.map ~f:( ! ) ports.source.before_edge in
     assert (Bool.equal !is_first_word (Bits.to_bool stream.valid));
     let this_word =
       let popcount = Bits.to_unsigned_int (Bits.popcount stream.value.tkeep) in
       Bits.sel_bottom ~width:(popcount * 8) stream.value.tdata
     in
     Vec.push_back collected this_word;
-    if not (Bits.to_bool stream.value.tlast) then 
-      loop ()
+    if not (Bits.to_bool stream.value.tlast) then loop ()
   in
   loop ();
   Vec.to_list collected
   |> List.map ~f:(fun x -> Constant.Raw.to_string (Bits.to_constant x))
   |> String.concat ~sep:""
-;;
--- test/stream_simulator.mli
+++ test/stream_simulator.mli
@@ -1,37 +1,31 @@
 open Core
-open Hardcaml 
-
+open Hardcaml
 module Stream = Xls_demo_lib.Stream
 module Step = Hardcaml_step_testbench_effectful.Imperative.Cyclesim
-module Before_and_after_edge = Hardcaml_step_testbench_effectful.Before_and_after_edge
+
+module Before_and_after_edge =
+  Hardcaml_step_testbench_effectful.Before_and_after_edge
 
 module Send_ports : sig
-  type t =
-    { source : Bits.t ref Stream.With_valid.t
-    ; ready  : Bits.t ref Before_and_after_edge.t
-    }
+  type t = {
+    source : Bits.t ref Stream.With_valid.t;
+    ready : Bits.t ref Before_and_after_edge.t;
+  }
 end
 
 module Receive_ports : sig
-  type t =
-    { source : Bits.t ref Stream.With_valid.t Before_and_after_edge.t 
-    ; ready  : Bits.t ref 
-    }
+  type t = {
+    source : Bits.t ref Stream.With_valid.t Before_and_after_edge.t;
+    ready : Bits.t ref;
+  }
 end
 
 type config =
-  | Always_high 
+  | Always_high
   | Rng of { rng : Random.State.t; probability_high : float }
 
-val send_packet
-   : ?config: config 
-   -> Step.Handler.t @ local
-  -> Send_ports.t
-  -> string
-  -> unit
+val send_packet :
+  ?config:config -> Step.Handler.t @ local -> Send_ports.t -> string -> unit
 
-val receive_packet
-   : ?config: config 
-  -> Step.Handler.t @ local
-  -> Receive_ports.t
-  -> string
+val receive_packet :
+  ?config:config -> Step.Handler.t @ local -> Receive_ports.t -> string
